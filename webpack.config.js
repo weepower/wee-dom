f2c37b2420de088ef427fad2635e8ec807dbe486
Nathan Hood <nathanhood@me.com>
2017-03-18 10:46:48 -0500

    Figure out how to tree shake with babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,22 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: env === 'prod' ? 'wee-dom.min.js' : 'wee-dom.js',
 		libraryTarget: 'umd'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: {
+					presets: [
+						[
+							'es2015',
+							{ modules: false } // Stops es2015 preset from generating commonjs modules
+						]
+					]
+				}
+			}
+		]
 	}
 };
