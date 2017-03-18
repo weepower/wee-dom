a9bc0c6ea9a46c87c3b02e975c9bd4fcf6a41bfe
Nathan Hood <nathanhood@me.com>
2017-03-18 11:10:14 -0500

    Correct path resolution for webpack tests

--- tests/webpack.config.js
+++ tests/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-
+console.error(__dirname, "/../node_modules");
 module.exports = {
 	resolve: {
 		modules: [
-			path.resolve(__dirname + '/../lib'),
-			path.resolve(__dirname, "./node_modules")
+			path.resolve(__dirname, '../lib'),
+			path.resolve(__dirname, '../node_modules')
 		]
 	}
 };
